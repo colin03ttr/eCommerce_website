1f020abccb110dcdf50d60e7426c6a83443cd4b6
izma26 <ismaeltir14@gmail.com>
2024-12-12 11:31:53 +0100

    new db

--- backend/dist/sequelize.js
+++ backend/dist/sequelize.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 // Configuration de la base de données
-const sequelize = new sequelize_1.Sequelize('mini_site_database', 'mini_site_owner', 'colin', {
+const sequelize = new sequelize_1.Sequelize('db', 'postgres', 'root', {
     host: 'localhost',
     dialect: 'postgres', // Utilisation de PostgreSQL
     port: 5432, // Port PostgreSQL par défaut
