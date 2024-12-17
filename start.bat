d10e7217fd4e65b55a464cb518e96f47b9903b38
colin03ttr <colin.tanter@edu.devinci.fr>
2024-12-17 18:50:14 +0100

    start.bat no npx tsc

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 echo Lancement du backend et du frontend...
 
 :: Lancer le backend dans une nouvelle fenêtre de commande
-start cmd /k "cd .\backend && npx tsc && node .\dist\app.js"
+start cmd /k "cd .\backend && node .\dist\app.js"
 
 :: Lancer le frontend dans une nouvelle fenêtre de commande
 start cmd /k "cd .\frontend && ng serve"
